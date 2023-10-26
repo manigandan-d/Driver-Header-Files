4c3c53b0f0a336df644dedf60f753272b8298208
MD <81799002+manigandan-d@users.noreply.github.com>
2023-10-26 15:22:43 +0530

    Create stm32f407xx_rcc_driver.h

---
+++ stm32f407xx_rcc_driver.h
@@ -0,0 +1,9 @@
+#ifndef INC_STM32F407XX_RCC_DRIVER_H_
+#define INC_STM32F407XX_RCC_DRIVER_H_
+
+#include "stm32f407xx.h"
+
+uint32_t RCC_GetPCLK1Value(void);
+uint32_t RCC_GetPCLK2Value(void);
+
+#endif /* INC_STM32F407XX_RCC_DRIVER_H_ */
